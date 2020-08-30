fb04dce7141432b7b68dc16649508601248f57ad
CCortinas <crcortinas@gmail.com>
2020-08-30 00:43:07 -0500

    set environment variables for cloudinary upload.now able to successfully upload an image to cloudinary

--- imgrepo-be/index.js
+++ imgrepo-be/index.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
 
 const server = express();
 const upload = multer();
@@ -12,10 +14,35 @@ server.use(express.urlencoded({ extended: true }));
 server.use(upload.array("uploadFile"));
 server.use(express.static("public"));
 
-server.post("/", (req, res) => {
-  console.log(req.files);
+cloudinary.config({
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret,
+});
 
-  res.status(200).json({ message: "received a request" });
+server.get("/lib", (req, res) => {
+  res.status(200).json({
+    images: [
+      "http://res.cloudinary.com/dalvqmups/image/upload/v1598765845/u8ctbaigtgay9wkxkeyg.jpg",
+    ],
+  });
+});
+
+server.post("/upload", (req, res) => {
+  const img = req.files[0];
+  const base64img = img.buffer.toString("base64");
+
+  cloudinary.uploader.upload(
+    `data:${img.mimetype};base64,${base64img}`,
+    (error, result) => {
+      if (error) {
+        console.log("error", error);
+        res.status(500).json({ error });
+      } else {
+        res.status(201).json({ message: "received a request", result });
+      }
+    }
+  );
 });
 
 server.listen(8800, () => {

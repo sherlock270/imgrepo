ecb5c647f920aeb8760e8e8ba3299998025d6fc7
CCortinas <crcortinas@gmail.com>
2020-09-07 19:19:14 -0500

    Navigating to Dashboard and image details requires token validation. If the token doesn't exist or is invalid the stored token is set to an empty string

--- imgrepo-fe/src/App.js
+++ imgrepo-fe/src/App.js
@@ -1,24 +1,58 @@
 import React from "react";
 import "./App.css";
-import { Route } from "react-router-dom";
+import { Route, Redirect } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import Landing from "./components/Landing";
 import Header from "./components/Header";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import ImageDetail from "./components/ImageDetail";
+import axios from "axios";
 
 function App() {
   return (
     <div className="app-container">
       <Route path="/" component={Header} />
       <Route exact path="/" component={Landing} />
-      <Route path="/dashboard" component={Dashboard} />
+      <Route
+        path="/dashboard"
+        render={() => {
+          if (isLoggedIn()) {
+            return <Dashboard />;
+          } else {
+            alert("You're not logged in");
+            localStorage.setItem("token", "");
+            return <Redirect to="/" />;
+          }
+        }}
+      />
       <Route path="/register" component={Register} />
       <Route path="/login" component={Login} />
-      <Route path="/image" component={ImageDetail} />
+      <Route
+        path="/image"
+        render={() => {
+          if (isLoggedIn()) {
+            return <ImageDetail />;
+          } else {
+            localStorage.setItem("token", "");
+            alert("You're not logged in");
+            return <Redirect to="/" />;
+          }
+        }}
+      />
     </div>
   );
+}
+
+function isLoggedIn() {
+  const token = localStorage.getItem("token");
+  if (token && token.length > 8) {
+    axios.post("https://img-repo.herokuapp.com/verify", token).then((res) => {
+      return res.loggedIn;
+    });
+  } else {
+    return false;
+  }
 }
 
 export default App;
